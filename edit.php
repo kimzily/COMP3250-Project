3257d318dca3575407def6bd3f9f739e178423be
mwilli10 <williamsmorgan123@gmail.com>
2015-03-03 18:18:27 -0600

    Update edit.php
    css and formatting not included

--- edit.php
+++ edit.php
@@ -1,28 +1,9 @@
 <!DOCTYPE html>
 
  <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>ICOT Sign Up</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width">
-
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <style>
-			.error {color: #FF0000;}
-            body {
-                padding-bottom: 20px;
-				padding-top: 35px;
-            }
-			
-		</style>
+        
 		
-        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="css/main.css">
-
-        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-		
-    </head>
+  </head>
 
 
   <script>
@@ -49,24 +30,21 @@ function validateAll(){
 
   }
 
-
   x=document.forms["form"]["phone"].value;
-  if (x!=""){
-  	 
-    var phoneno = /^\d{10}$/;
-    alert("");
-    if (x.value.match(phoneno)) {
-   		alert("The phone number contains illegal characters.");
-        return false;
+  if (x!="0"){
+  	if (x.length != 10){
+      alert("Invalid phone number");
+            return false;
+
+    }
+    
         
-    } else if (!(stripped.length == 10)) {
-    	alert("The phone number is too short.");
-        return false;
+  } 
         
-    	} 
+    
   }
 
- }
+ 
 
 
 function validatePhone(fld) {
@@ -93,19 +71,19 @@ function validatePhone(fld) {
 
 
 <?php
- include 'connection.php';
+ require'connection.php';
 
-if (isset($_GET['edit'])){
-	$id = $_GET['edit'];
+//if (isset($_GET['edit'])){
+//	$id = $_GET['edit'];
+  $id = '1';
 
-
-		$userquery = mysql_query("SELECT * FROM members WHERE id='$id'");
-		while ($row = mysql_fetch_array($userquery, MYSQL_ASSOC)){
-			$id = $row['memberid'];
-			$firstname = $row['firstname'];
-			$lastname = $row['lastname'];
+		$userquery = mysqli_query($con,"SELECT * FROM memberInfo WHERE id='$id'") or die("The query could not be completed. Please try again later".mysqli_error($con));
+		while ($row = mysqli_fetch_array($userquery, MYSQL_ASSOC)){
+      $dbusername = $row['username'];
+			$id = $row['id'];
+			$fname = $row['firstname'];
+			$lname = $row['lastname'];
 			$email = $row['email'];
-			$dbusername = $row['username'];
 			$address = $row['address'];
 			$phone = $row['phone'];
 
@@ -120,12 +98,12 @@ if (isset($_GET['edit'])){
 		$newPhone = $_POST['phone'];
 
 
-		$sql = "UPDATE members SET firstname='$newFName' , lastname='$newLName', email = '$newEmail', address = '$newAddress' , phone = '$newPhone' WHERE id='$id'";
-		$res = mysql_query($sql) 
-                                    or die("Could not update".mysql_error());
+		$sql = "UPDATE memberInfo SET firstname='$newFName' , lastname='$newLName', email = '$newEmail', address = '$newAddress' , phone = '$newPhone' WHERE id='$id'";
+		$res = mysqli_query($con,$sql) 
+                                    or die("Could not update".mysqli_error($con));
 		echo "<meta http-equiv='refresh' content='0;url=profile.php'>";
 	}
-}
+//}
  
 ?>
 
@@ -133,19 +111,21 @@ if (isset($_GET['edit'])){
 
 <h2>Profile Update</h2>
 <p><span class="error">* Denotes a required field </span></p> </br>
-<form name="form" method="post" action="profile.php" onsubmit="return validateAll();">
+<p><span class="error">In order to update information simply change the information and click "Update" </span></p> </br>
+<form name="form" method="post" action="edit.php" onsubmit="return validateAll();">
 <table>
    <tr><td>*First Name:</td><td><input type="text" name="fname" value="<?php echo $fname;?>"></br>
-   <span class="error">* <?php echo $nameErr;?></span></td></tr>
+   
 
    <tr><td>*Last Name:</td><td><input type="text" name="lname" value="<?php echo $lname;?>"></br>
-   <span class="error">* <?php echo $nameErr;?></span></td></tr>
+   
 
    <tr><td>*Email:</td><td><input type="text" name="email" value="<?php echo $email;?>"></br>
-   <span class="error">* <?php echo $nameErr;?></span></td></tr>
+ 
 
-   <tr><td>Phone Number:</td><td><input type="text" name="phone" value="<?php echo $phone;?>"></br>
+   <tr><td>Phone Number:</td><td><input type="number" name="phone" value="<?php echo $phone;?>"></br>
    
+
    <tr><td>Address:</td><td><input type="text" name="address" value="<?php echo $address;?>"></br>
    
  
@@ -159,17 +139,6 @@ if (isset($_GET['edit'])){
 
 
 </html>
-
-
-
-
-
-
-
-
-
-
-
 
 
 
